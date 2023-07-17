37762eb02f05c111e03424c7c0fbfc2c6667832e
Joao-Faggion <joao.faggion@limbersoftware.com.br>
2023-07-17 08:36:03 -0300

    feito merge

--- src/app/component/form/form.component.ts
+++ src/app/component/form/form.component.ts
@@ -5,6 +5,37 @@ import { Component } from '@angular/core';
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
-export class FormComponent {
+export class FormComponent implements OnInit {
+
+  pais =['Brasil', 'Australia', 'Argentina', 'Alemanha', 'Peru', 'Uruguai', 'Bolívia', 'Angola', 'Estados Unidos', 'Canadá', 'França', 'Espanha', 'Congo', 'Armênia'];
+
+  constructor(private builder: FormBuilder) { }
+
+  ngOnInit(): void {
+
+  }
+
+  userForm = this.builder.group({
+    nome: this.builder.control('', Validators.required),
+    sobrenome: this.builder.control('', Validators.required),
+    datanasc: this.builder.control('', Validators.required),
+    email: this.builder.control('', Validators.compose([Validators.required, Validators.required])),
+    endereco: this.builder.control('', Validators.required),
+    senha: this.builder.control('', Validators.required),
+    complemento: this.builder.control('', Validators.required),
+    pais: this.builder.control('', Validators.required),
+    telefone: this.builder.control('', Validators.required),
+    genero: this.builder.control('Masculino'),
+  })
+
+  SalvarUser(){
+    console.log(this.userForm.value)
+  }
+
+  LimparForm(){
+    this.userForm.reset();
+  }
+
+
 
 }
